e2c5a03e56a560ec1d4fc879b6600f991272176d
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-06 22:09:50 +0530

    validate, auth, sign in and sign out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,131 @@
-import React from 'react'
-import logo from '../assets/logo_netflix.png';
+// import { onAuthStateChanged, signOut } from "firebase/auth";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { LOGO } from "../utils/constants";
+// import { auth } from "../utils/firebase";
+// import { addUser, removeUser } from "../utils/userSlice";
+
+// const Header = () => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const user = useSelector((store) => store.user);
+//   const handleSignOut = () => {
+//     signOut(auth)
+//       .then(() => {})
+//       .catch((error) => {
+//         navigate("/error");
+//       });
+//   };
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         const { uid, email, displayName, photoURL } = user;
+//         dispatch(
+//           addUser({
+//             uid: uid,
+//             email: email,
+//             displayName: displayName,
+//             photoURL: photoURL,
+//           })
+//         );
+//         navigate("/browse");
+//       } else {
+//         dispatch(removeUser());
+//         navigate("/");
+//       }
+//     });
+//     return () => unsubscribe();
+//   }, []);
+
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LOGO } from "../utils/constants";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
+import userImg from '../assets/user.jpg';
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("onAuthStateChanged user:", user); // Log user data
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch, navigate]);
+
+  useEffect(() => {
+    console.log("Redux user state:", user); // Log Redux user state
+  }, [user]);
+
   return (
-    <div className = "absolute px-8 py-2 bg-gradient-to-b from-black z-10">
-        <img 
-        className = "w-44"
-        src = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt ="logo"/>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+      {user && (
+        <div className="flex p-2 justify-between">
+          <img
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+            src={userImg}
+          />
+          <button onClick={handleSignOut} className="w-8 h-8 rounded-md ml-6">
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
+
+
+
+//   return (
+//     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+//       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+//       {user && (
+//         <div className="flex p-2 justify-between">
+//           <img
+//             className="hidden md:block w-12 h-12"
+//             alt="usericon"
+//             src={user?.photoURL}
+//           />
+//           <button onClick={handleSignOut} className="w-8 h-8 rounded-md ml-6">
+//             Sign Out
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+// export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,134 @@
-import {React,useState} from 'react'
-import Header from './Header'
-import { Simulate } from 'react-dom/test-utils';
+import { useState, useRef } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const dispatch = useDispatch();
 
-   const toggleSignInForm = ()=>{
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
+
+    if (!isSignInForm) {
+      // Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: USER_AVATAR,
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      // Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          //const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
+
+  const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
-   }
+  };
   return (
     <div>
       <Header />
       <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/21a8ba09-4a61-44f8-8e2e-70e949c00c6f/6678e2ea-85e8-4db2-b440-c36547313109/IN-en-20240722-POP_SIGNUP_TWO_WEEKS-perspective_WEB_3457a8b1-284d-4bb5-979e-2a2e9bb342b3_large.jpg" alt="background" />
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        
-        {! isSignInForm && <input type="text" placeholder='Full Name' className="p-4 my-4 w-full bg-gray-700" />}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
 
-        <input type="text" placeholder='Email' className="p-4 my-4 w-full bg-gray-700" />
-        <input type="password" placeholder='Password' className='p-4 my-4 w-full bg-gray-700' />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
-        <p className = "py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New User ? Sign Up" : "Already user? Sign In"}</p>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
 };
 
-export default Login
+export default Login;
